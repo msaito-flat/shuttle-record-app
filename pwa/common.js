3cacdff4e80fb3a3e94e93f2a4b18d57298977a0
msaito-flat <m.saito@flat.or.jp>
2026-02-15 18:10:02 +0900

    Rel: Bump version to v1.0.15

--- pwa/common.js
+++ pwa/common.js
@@ -3,7 +3,7 @@
 
 // CONFIGURATION
 const API_URL = 'https://script.google.com/macros/s/AKfycbwj5ZNC3gTMZzMsjFGatdOfFn6o7GOGSHPfImS1Dcj_BRDTKmunOzsNLTXVBWexMg/exec';
-const APP_VERSION = 'v1.0.14'; // Display Version
+const APP_VERSION = 'v1.0.15'; // Display Version
 
 // STATE MANAGEMENT
 const Store = {
--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kirari-shuttle-v14';
+const CACHE_NAME = 'kirari-shuttle-v15';
 const ASSETS = [
     './',
     './index.html',
